560d21ac28f5cda30041696630a3d4cf959150a3
TakaBro <lekt@icomp.ufam.edu.br>
2018-02-14 21:20:05 -0400

    First HTTP Request Test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 import { TestPage } from '../test/test';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private url:string = 'http://localhost:3030/v1'
+  public beers: Array<{}>;
 
+  constructor(
+      public navCtrl: NavController,
+      public http: Http
+  ) {
+    this.http.get(this.url + '/beers')
+             .map(res => res.json())
+             .subscribe(data  => {
+               console.log(data);
+             });
   }
 
   goToTestPage(){
